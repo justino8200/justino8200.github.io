8e3eb0b3d9061da8fcc2a97dfa1ff79d656b67d7
justino8200 <43251083+justino8200@users.noreply.github.com>
2018-10-10 11:41:53 -0400

    Create 2018-10-05-weekly-reflection-01.m

---
+++ _posts/2018-10-05-weekly-reflection-01.m
@@ -0,0 +1,7 @@
+---
+layout: post
+title: Justin O'Reilly weekly reflection
+date: 2018-10-05
+---
+
+I came to this school looking forward to learn about computers and coding etc and that is what Mr.Allatta is teaching me and peers. Me and my partner still haven't decided on an idea but we really looking forward to create this game.
